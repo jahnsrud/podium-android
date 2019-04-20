dc82dddbbb783de77f6506d3a2f57fa89f094458
Markus Jahnsrud <markus@soriko.no>
2019-04-20 19:13:24 +0200

    Important fixes!

--- app/src/main/java/no/jahnsrud/podium/MainActivity.kt
+++ app/src/main/java/no/jahnsrud/podium/MainActivity.kt
@@ -2,21 +2,31 @@ package no.jahnsrud.podium
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
+            R.id.libraryFragment-> {
                 textMessage.setText(R.string.title_home)
+
+
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
+            R.id.searchFragment -> {
                 textMessage.setText(R.string.title_search)
+
+
+                val manager = supportFragmentManager
+                val transaction = manager.beginTransaction()
+                transaction.add(R.layout.fragment_search, this.fragment)
+                transaction.commit()
 
 
                 return@OnNavigationItemSelectedListener true
@@ -26,6 +36,8 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
         }
+
+
         false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
