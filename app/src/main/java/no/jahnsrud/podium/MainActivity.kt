93088f3e73f5366cdfd737e183beb53f0fa3283f
Markus Jahnsrud <markus@soriko.no>
2019-04-20 17:10:36 +0200

    Added the famous bottom navigation. More soon!

--- app/src/main/java/no/jahnsrud/podium/MainActivity.kt
+++ app/src/main/java/no/jahnsrud/podium/MainActivity.kt
@@ -3,27 +3,42 @@ package no.jahnsrud.podium
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.view.Menu
 import android.view.View
 import no.jahnsrud.podium.Models.Podcast
 
 class MainActivity : AppCompatActivity() {
 
+    var textMessage:String = ""
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                // textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                // textMessage.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                // textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun openPlaybackActivity(view: View) {
-        val intent = Intent(this, PlaybackFragment::class.java)
-        startActivity(intent)
-    }
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-    fun openSearch(view: View) {
-
-        val intent = Intent(this, SearchFragment::class.java)
-        startActivity(intent)
-
+        // textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 
 
