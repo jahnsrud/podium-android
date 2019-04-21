7230f3f9e281f958b064d4eccf52eacd530d7633
Markus Jahnsrud <markus@soriko.no>
2019-04-21 17:00:11 +0200

    HUGE improvements. More soon, just have to commit before trying something cool 😎

--- app/src/main/java/no/jahnsrud/podium/MainActivity.kt
+++ app/src/main/java/no/jahnsrud/podium/MainActivity.kt
@@ -1,95 +1,62 @@
 package no.jahnsrud.podium
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
-import android.view.View
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.fragment_library.*
+import android.view.MenuItem
+import no.jahnsrud.podium.Navigation.TabManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener  {
 
-    val fragment1: Fragment = LibraryFragment()
-    val fragment2: Fragment = SearchFragment()
-    val fragment3: Fragment = SettingsFragment()
-    val fm = supportFragmentManager
-    var active = fragment1
+    /**
+     * Much of the Tab Bar logic is inspired and by the MultiNavProject by moallemi
+     * I adapted much of the logic, but made improvements too
+     * https://github.com/moallemi/MultiNavHost
+     */
 
+    private val tabManager: TabManager by lazy { TabManager(this) }
 
-    @SuppressLint("ResourceType")
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.libraryFragment-> {
-
-                runOnUiThread {
-                    fm.beginTransaction().hide(active).show(fragment1).commit();
-                    active = fragment1;
-                    // your code to update the UI thread here
-                }
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.searchFragment -> {
-
-                runOnUiThread {
-                    fm.beginTransaction().hide(active).show(fragment2).commit();
-                    active = fragment2;
-
-                }
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-
-                runOnUiThread {
-                    fm.beginTransaction().hide(active).show(fragment3).commit();
-                    active = fragment3;
-
-                }
-
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-
-        false
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContentView(R.layout.activity_main)
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        tabBar.setOnNavigationItemSelectedListener(this)
 
-        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
-        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
-        fm.beginTransaction().add(R.id.main_container,fragment1, "1").commit();
+        if (savedInstanceState == null) {
+            tabManager.currentController = tabManager.libraryTabController
+        }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
 
+        tabManager.onSaveInstanceState(outState)
+    }
 
-        // navView.setupWithNavController(Navigation.findNavController(this, R.id.main_container))
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
 
+        tabManager.onRestoreInstanceState(savedInstanceState)
+    }
 
+    override fun supportNavigateUpTo(upIntent: Intent) {
+        tabManager.supportNavigateUpTo(upIntent)
+    }
 
+    override fun onBackPressed() {
+        tabManager.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        tabManager.switchTab(menuItem.itemId)
+        return true
     }
 
 
-    fun openPlayback(view: View) {
-        val intent = Intent(this@MainActivity, PlaybackActivity::class.java)
-        startActivity(intent)
-    }
+
 }
---
+++ app/src/main/java/no/jahnsrud/podium/Navigation/TabManager.kt
@@ -0,0 +1,118 @@
+package no.jahnsrud.podium.Navigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isInvisible
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import no.jahnsrud.podium.MainActivity
+import no.jahnsrud.podium.R
+
+class TabManager(private val mainActivity: MainActivity) {
+
+    private val startDestinations = mapOf(
+            R.id.tab_library to R.id.libraryFragment,
+            R.id.tab_search to R.id.searchFragment,
+            R.id.tab_settings to R.id.settingsTab
+    )
+    private var currentTabId: Int = R.id.tab_library
+    var currentController: NavController? = null
+    private var tabHistory = TabHistory().apply { push(R.id.tab_library) }
+
+    val libraryTabController: NavController by lazy {
+        mainActivity.findNavController(R.id.libraryTab).apply {
+            graph = navInflater.inflate(R.navigation.nav_graph).apply {
+                startDestination = startDestinations.getValue(R.id.tab_library)
+            }
+        }
+    }
+    private val searchTabController: NavController by lazy {
+        mainActivity.findNavController(R.id.searchTab).apply {
+            graph = navInflater.inflate(R.navigation.nav_graph).apply {
+                startDestination = startDestinations.getValue(R.id.tab_search)
+            }
+        }
+    }
+    private val settingsTabController: NavController by lazy {
+        mainActivity.findNavController(R.id.settingsTab).apply {
+            graph = navInflater.inflate(R.navigation.nav_graph).apply {
+                startDestination = startDestinations.getValue(R.id.tab_settings)
+            }
+        }
+    }
+
+    private val homeTabContainer: View by lazy { mainActivity.libraryTabContainer }
+    private val dashboardTabContainer: View by lazy { mainActivity.searchTabContainer }
+    private val notificationsTabContainer: View by lazy { mainActivity.settingsTabContainer }
+
+    fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putSerializable(KEY_TAB_HISTORY, tabHistory)
+    }
+
+    fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        savedInstanceState?.let {
+            tabHistory = it.getSerializable(KEY_TAB_HISTORY) as TabHistory
+
+            switchTab(mainActivity.tabBar.selectedItemId, false)
+        }
+    }
+
+    fun switchTab(tabId: Int, addToHistory: Boolean = true) {
+        currentTabId = tabId
+
+        when (tabId) {
+            R.id.tab_library -> {
+                currentController = libraryTabController
+                invisibleTabContainerExcept(homeTabContainer)
+            }
+            R.id.tab_search -> {
+                currentController = searchTabController
+                invisibleTabContainerExcept(dashboardTabContainer)
+            }
+            R.id.tab_settings -> {
+                currentController = settingsTabController
+                invisibleTabContainerExcept(notificationsTabContainer)
+            }
+        }
+        if (addToHistory) {
+            tabHistory.push(tabId)
+        }
+    }
+
+    fun supportNavigateUpTo(upIntent: Intent) {
+        currentController?.navigateUp()
+    }
+
+    fun onBackPressed() {
+        currentController?.let {
+            if (it.currentDestination == null || it.currentDestination?.id == startDestinations.getValue(currentTabId)) {
+                if (tabHistory.size > 1) {
+                    val tabId = tabHistory.popPrevious()
+                    switchTab(tabId, false)
+                    mainActivity.tabBar.menu.findItem(tabId)?.isChecked = true
+                } else {
+                    mainActivity.finish()
+                }
+            }
+            it.popBackStack()
+        } ?: run {
+            mainActivity.finish()
+        }
+    }
+
+
+
+    private fun invisibleTabContainerExcept(container: View) {
+        homeTabContainer.isInvisible = true
+        dashboardTabContainer.isInvisible = true
+        notificationsTabContainer.isInvisible = true
+
+        container.isInvisible = false
+    }
+
+    companion object {
+        private const val KEY_TAB_HISTORY = "key_tab_history"
+    }
+}
