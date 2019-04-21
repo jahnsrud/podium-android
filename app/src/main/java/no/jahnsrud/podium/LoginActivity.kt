710b26867aef28cbc1815f326364245215df5481
Markus Jahnsrud <markus@soriko.no>
2019-04-21 22:45:27 +0200

    Added LoginActivity

---
+++ app/src/main/java/no/jahnsrud/podium/LoginActivity.kt
@@ -0,0 +1,12 @@
+package no.jahnsrud.podium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
