d67766b34c9f1ada125d5e3b743d10b7536ea0ad
Markus Jahnsrud <markus@soriko.no>
2019-04-28 21:26:17 +0200

    Much prettier login experience

--- app/src/main/java/no/jahnsrud/podium/activities/LoginActivity.kt
+++ app/src/main/java/no/jahnsrud/podium/activities/LoginActivity.kt
@@ -1,7 +1,9 @@
 package no.jahnsrud.podium.activities
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import no.jahnsrud.podium.R
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +17,10 @@ class LoginActivity : AppCompatActivity() {
         super.onStart()
 
         getSupportActionBar()?.hide();
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
 
     }
 }
